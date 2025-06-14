3bc9208066aceaf56dffd5fa8ea267e9def4980d
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-06-14 21:12:48 +0530

    Install React Router DOM and configure basic routing structure.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+/*import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/SignUp';*/
+import DashboardPage from './pages/DashboardPage';
+/*import TaskListPage from './pages/TaskListPage';
+import TaskDetailsPage from './pages/TaskDetailsPage';
+import AddEditTaskPage from './pages/AddEditTaskPage';
+import UserProfilePage from './pages/UserProfilePage';
+import UserManagementPage from './pages/UserManagementPage';*/
+import NotFoundPage from './pages/404Page';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                {/*<Route path="/" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />*/}
+                <Route path="/" element={<DashboardPage />} />
+{/*                <Route path="/tasks" element={<TaskListPage />} />
+                <Route path="/tasks/:id" element={<TaskDetailsPage />} />
+                <Route path="/tasks/edit/:id" element={<AddEditTaskPage />} />
+                <Route path="/tasks/add" element={<AddEditTaskPage />} />
+                <Route path="/profile" element={<UserProfilePage />} />
+                <Route path="/admin/users" element={<UserManagementPage />} />*/}
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </Router>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
